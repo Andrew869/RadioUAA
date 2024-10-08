c5585df68e14d5a0e29eb7cc738836e9364d69f5
cryptoboys-cyber <vicente2.0luis@gmail.com>
2024-10-07 19:14:24 -0600

    se agrgan estilos

--- radio-admin/index.php
+++ radio-admin/index.php
@@ -29,6 +29,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Radio Admin</title>
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
     <header>
@@ -37,25 +38,38 @@
     ?>
     </header>
     <aside></aside>
-    <main>
-        <div>
-            programan en vivo
+    <main class="main">
+        <div class="en_vivo">
+            <h1>Programas En vivo</h1>
         </div>
-        <div>
-            ultimos programas añadidos
+        <div class="ultimos_anadidos">
+            <h1>Anadidos Ultimamente</h1>
+            <div class="contenidos">
+                <div class="programas">
+                    <h2>PROGRAMAS</h2>
+                </div>
+
+                <div class="presentadores">
+                    <h2>PRESENTADORES</h2>
+                </div>
+
+                 <div class="generos">
+                    <h2>GENEROS</h2>
+                </div>
+
+            </div>
+            
+
         </div>
-        <div>
-            ultimos presentadores añadidos
-        </div>
-        <div>
-            utimos generos añadidos
-        </div>
-        <div>
-            ultimos comentarios publicados
+       
+       
+        <div class="comentarios">
+            <h2>Ultimos Comentarios</h2>
+            <div class="comentarios-con"></div>
         </div>
     </main>
     <footer>
-        TODO: footer
+
     </footer>
 </body>
 </html>
