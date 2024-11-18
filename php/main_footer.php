6745532c7fdb1351308a859ce45b7efa174590e9
Andrew869 <handres_rock@hotmail.com>
2024-11-18 15:22:32 -0600

    Merge branch 'EduardoPruebasWeb'

--- php/main_footer.php
+++ php/main_footer.php
@@ -12,14 +12,20 @@
         <a href="https://open.spotify.com/show/33OclgjRhmrjS1GaSAwXoU" target="_blank" class="social-button spotify">
             <?php echo GetSVG('resources/img/svg/spotify.svg', ["24px", "24px", "white"]) ?>
         </a>
-        <!-- <a href="https://play.google.com/store/search?q=radio+uaa+94.5fm&c=apps&pli=1" target="_blank" class="social-button google-play">
-            <?php //echo GetSVG('resources/img/svg/google-play.svg', ["20px", "20px", "white"]) ?>
-        </a>
-        <a href="https://apps.apple.com/mx/app/radio-uaa-94-5fm/id6670407197"target="_blank" class="social-button apple-store">
-            <?php //echo GetSVG('resources/img/svg/app-store.svg', ["24px", "24px", "white"]) ?>
-        </a> -->
     </div>
 </div>
+
+<div class="container-descarga-buttons">
+    <div class="descarga-buttons">
+        <a href="https://play.google.com/store/search?q=radio+uaa+94.5fm&c=apps&pli=1" target="_blank" class="social-button google-play">
+            <?php echo GetSVG('resources/img/svg/google-play.svg', ["20px", "20px", "white"]) ?>
+        </a>
+        <a href="https://apps.apple.com/mx/app/radio-uaa-94-5fm/id6670407197"target="_blank" class="social-button apple-store">
+            <?php echo GetSVG('resources/img/svg/app-store.svg', ["24px", "24px", "white"]) ?>
+        </a>
+    </div>
+</div>
+
 
 <?php include 'php/player.php' ?>
 
@@ -31,17 +37,14 @@
         <a href="https://www.uaa.mx/dgdv/" target="_blank">
             <img src="resources/img/UAA-DG.png" alt="Logo UAA" class="logo-uaa" />
         </a>
-        <!-- <a href="https://www.uaa.mx/portal/comunicacion/radio-y-tv/" target="_blank">
-                    <img src="resources/img/UAA-TV-RADIO.png" alt="Logo UAA" class="logo-uaa">
-                </a> -->
     </div>
     <div class="copyright-container">
         <div class="copyright">
             <p>&copy; Derechos de autor 1978-2024 Radio UAA – Proyección de la voz universitaria</p>
         </div>
         <div class="copyright-links">
-            <a class="internal-link" href="politica-de-privacidad">Política de privacidad</a>
-            <a class="internal-link" href="transparencia">Transparencia</a>
+            <a class="internal-link" href="politica-de-privacidad"> <span style="color: white;"><b>| Politica de Privacidad |</span>.</b></a>
+            <a class="internal-link" href="transparencia"> <span style="color: white;"><b>Transparencia</span>.</b></a>
         </div>
     </div>
 </footer>
